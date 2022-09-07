616f45c021616738725ab8fa81a26ef8ac6c98d3
Rueslei Santos <dudui1998@gmail.com>
2022-09-06 21:37:18 -0300

    FrontEnd criado

---
+++ Models/Endereco.cs
@@ -0,0 +1,20 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MeuWebApp.Models
+{
+    public class Endereco
+    {
+        [Required(ErrorMessage ="O campo {0} é de preenchimento obrigatório.")]
+        [RegularExpression(@"[0-9]{8}$", ErrorMessage ="O campo {0} deve ser preenchio com 8 dígitos numéricos.")]
+        public int CEP { get; set; }
+
+        public string Estado { get; set; }
+         
+        public string Cidade { get; set; }
+
+        public string Bairro { get; set; }
+
+        public string Logradouro { get; set; }
+            
+    }
+}
---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,22 @@
+﻿using MeuWebApp.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MeuWebApp.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+        Endereco Endereco = new Endereco();
+        public void OnGet()
+        {
+        }
+
+    
+    }
+}
